03e0fddd700d4deebbd33807cc44fd11229df591
kanzariyapratik007 <kanzariyapratik124@gmail.com>
2026-09-15 14:47:50 +0530

    Move Gunicorn socket to /tmp/pwbackend.sock to eliminate Nginx 502

--- deploy/setup_aws_ec2.sh
+++ deploy/setup_aws_ec2.sh
@@ -39,7 +39,7 @@ After=network.target
 [Service]
 User=${USER_NAME}
 WorkingDirectory=${APP_DIR}
-ExecStart=${APP_DIR}/venv/bin/gunicorn --access-logfile - --umask 000 --workers 3 --bind unix:${APP_DIR}/pwbackend.sock pwbackend.wsgi:application
+ExecStart=${APP_DIR}/venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:/tmp/pwbackend.sock pwbackend.wsgi:application
 
 [Install]
 WantedBy=multi-user.target
@@ -63,7 +63,7 @@ server {
 
     location / {
         include proxy_params;
-        proxy_pass http://unix:${APP_DIR}/pwbackend.sock;
+        proxy_pass http://unix:/tmp/pwbackend.sock;
     }
 }
 EOF
